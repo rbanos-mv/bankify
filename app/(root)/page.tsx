d6e032ebd9cf5b49a2eeadd00c656b6954e20cf5
Roberto A. Baños Alvarez <roberto.banos.mv@gmail.com>
2024-06-27 17:31:29 +0000

    create HeaderBox and add it to the HomePage

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,6 +1,20 @@
+import HeaderBox from '@/components/HeaderBox';
+
 const Home = async () => {
+  const loggedIn = { firstName: 'Roberto', lastName: 'Baños', email: 'roberto.banos@gmail.com' };
   return (
-    <div>Home</div>
+    <section className='home'>
+      <div className="home-content">
+        <header className='home-header'>
+          <HeaderBox
+            type='greeting'
+            title='Welcome'
+            user={loggedIn?.firstName || 'Guest'}
+            subtext='Access and manage your account and transactions efficiently.'
+          />
+        </header>
+      </div>
+    </section>
   )
 }
 
