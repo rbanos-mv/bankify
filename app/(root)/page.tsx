a30023d8633762176a754b481aa8bfc15b069f31
Roberto A. Baños Alvarez <roberto.banos.mv@gmail.com>
2024-06-27 18:17:41 +0000

    create TotalBalanceBox and its subcomponents AnimatedCounter and DoughnutChart; add it to the HomePage

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,7 +1,21 @@
 import HeaderBox from '@/components/HeaderBox';
+import TotalBalanceBox from '@/components/TotalBalanceBox';
 
 const Home = async () => {
   const loggedIn = { firstName: 'Roberto', lastName: 'Baños', email: 'roberto.banos@gmail.com' };
+  const accounts = {
+    data: [
+      { name: 'Plaid Checking', currentBalance: 1250.12, mask: '1111' },
+      { name: 'Plaid Savings', currentBalance: 2500.24, mask: '2222' },
+      { name: 'Plaid Credit', currentBalance: 3750.36, mask: '3333' }
+    ],
+    totalBanks: 0,
+    totalCurrentBalance: 0
+  };
+  const accountsData = accounts?.data as Account[];
+  accounts.totalBanks = accountsData.length;
+  accounts.totalCurrentBalance = accountsData?.reduce((accumulator, currentValue) => accumulator + currentValue.currentBalance, 0);
+
   return (
     <section className='home'>
       <div className="home-content">
@@ -11,6 +25,11 @@ const Home = async () => {
             title='Welcome'
             user={loggedIn?.firstName || 'Guest'}
             subtext='Access and manage your account and transactions efficiently.'
+          />
+          <TotalBalanceBox
+            accounts={accountsData}
+            totalBanks={accounts?.totalBanks}
+            totalCurrentBalance={accounts?.totalCurrentBalance}
           />
         </header>
       </div>
---
+++ components/DoughnutChart.tsx
@@ -0,0 +1,29 @@
+'use client';
+import { ArcElement, Chart as ChartJs, Legend, Tooltip } from 'chart.js';
+import { Doughnut } from 'react-chartjs-2';
+
+ChartJs.register(ArcElement, Tooltip, Legend);
+
+const DoughnutChart = ({ accounts }: DoughnutChartProps) => {
+  const accountNames = accounts.map((account) => account.name);
+  const balances = accounts.map((account) => account.currentBalance);
+  const data = {
+    datasets: [{
+      label: 'Balance',
+      data: balances,
+      backgroundColor: ['#0747b6', '#2265d8', '#2f91fa']
+    }],
+    labels: accountNames
+  }
+  return <Doughnut
+    data={data}
+    options={{
+      cutout: '60%',
+      plugins: {
+        legend: { display: false }
+      }
+    }}
+  />
+}
+
+export default DoughnutChart
